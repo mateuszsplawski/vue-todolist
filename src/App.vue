29253f2c8ee8dbcf074d354bf5a6ba8238940dd5
Mateusz Spławski <mateusz.splawski@netguru.com>
2022-08-07 19:36:36 +0200

    feat: add main logic

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script lang="ts">
 export default {
   data() {
-    return { content: { header: "Vue.js Todo list" } };
+    return {
+      content: {
+        header: "Vue.js Todo list",
+        inputLabel: "Todo name",
+        inputPlaceholder: "e.g. Create Vue.js app",
+        buttonText: "Add new Todo item",
+        listHeader: "Todos:",
+      },
+      inputValue: "",
+      todos: [],
+    };
+  },
+  methods: {
+    appendToDoList() {
+      this.todos.push(this.inputValue);
+      this.inputValue = "";
+    },
   },
 };
 </script>
@@ -12,6 +28,27 @@ export default {
       <header>
         <h1>{{ content.header }}</h1>
       </header>
+    </section>
+
+    <section>
+      <label
+        >{{ content.inputLabel }}
+        <input :placeholder="content.inputPlaceholder" v-model="inputValue" />
+      </label>
+
+      <button @click="appendToDoList">{{ content.buttonText }}</button>
+    </section>
+
+    <section>
+      <header>
+        <h2>{{ content.listHeader }}</h2>
+      </header>
+
+      <ul>
+        <li :key="todo" v-for="todo in todos">
+          {{ todo }}
+        </li>
+      </ul>
     </section>
   </main>
 </template>
