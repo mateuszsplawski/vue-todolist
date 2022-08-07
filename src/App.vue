a35f7b2014442907f8ee9c19fb43ebdaed5c7920
Mateusz Spławski <mateusz.splawski@netguru.com>
2022-08-07 19:14:03 +0200

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,17 @@
-<template>Hello world</template>
+<script lang="ts">
+export default {
+  data() {
+    return { content: { header: "Vue.js Todo list" } };
+  },
+};
+</script>
+
+<template>
+  <main>
+    <section>
+      <header>
+        <h1>{{ content.header }}</h1>
+      </header>
+    </section>
+  </main>
+</template>
